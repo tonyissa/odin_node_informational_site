9f0848544d678cb7414785d842e5907fe390f206
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-08-08 16:55:51 -0400

    Complete project

---
+++ index.js
@@ -0,0 +1,39 @@
+const http = require('http');
+const fs = require('fs/promises');
+
+const host = 'localhost';
+const port = 8000;
+
+const server = http.createServer(async function (req, res) {
+    if (req.url === '/') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        try {
+            const data = await fs.readFile('./index.html', 'utf8');
+            res.write(data);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            res.end();
+        }
+    } else if (req.url === '/about') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        try {
+            const data = await fs.readFile('./about.html', 'utf8');
+            res.write(data);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            res.end();
+        }
+    } else if (req.url === '/contact-me') {
+        res.writeHead(200, {'Content-Type': 'text/html'});
+        try {
+            const data = await fs.readFile('./contact-me.html', 'utf8');
+            res.write(data);
+        } catch (err) {
+            console.error(err);
+        } finally {
+            res.end();
+        }
+    }
+}).listen(8080);
