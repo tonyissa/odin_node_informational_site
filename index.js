697622ac1fa7b7153685c177ac6c746871de7f13
antwonthedamaja <52683702+antwonthedamaja@users.noreply.github.com>
2023-09-20 14:33:57 -0400

    Finish routing

--- index.js
+++ index.js
@@ -6,7 +6,19 @@ const port = 3000;
 app.use(express.static('public'))
 
 app.get('/', (req, res) => {
-    res.send()
+    res.sendFile(__dirname + '/public/index.html')
+})
+
+app.get('/contact-me', (req, res) => {
+    res.sendFile(__dirname + '/public/contact-me.html')
+})
+
+app.get('/about', (req, res) => {
+    res.sendFile(__dirname + '/public/about.html')
+})
+
+app.get('*', (req, res) => {
+    res.sendFile(__dirname + '/public/404.html')
 })
 
 app.listen(port, () => console.log(`App listening on port ${port}!`))
